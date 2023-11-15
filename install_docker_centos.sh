48d03b992e2536b38f89b100f1d839ff0d6d01ea
URDead <97973081+benjisho@users.noreply.github.com>
2023-11-16 00:01:07 +0200

    Update install_docker_centos.sh

--- install_docker_centos.sh
+++ install_docker_centos.sh
@@ -2,7 +2,6 @@
 
 # Script to Install Docker on CentOS
 
-# Step 1: Uninstall old versions of Docker
 echo "Uninstalling old Docker versions..."
 sudo yum remove -y docker \
                   docker-client \
@@ -13,20 +12,16 @@ sudo yum remove -y docker \
                   docker-logrotate \
                   docker-engine
 
-# Step 2: Set up the Docker repository
 echo "Setting up the Docker repository..."
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Step 3: Install Docker Engine, containerd, and plugins
 echo "Installing Docker Engine, containerd, and Docker plugins..."
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Step 4: Start Docker
 echo "Starting Docker..."
 sudo systemctl start docker
 
-# Step 5: Verify Installation
 echo "Verifying Docker installation..."
 sudo docker run hello-world
 
